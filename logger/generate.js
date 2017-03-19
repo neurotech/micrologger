775502a594583b50a1ffe070e79353111272a9c0
Tim Douglas <neurotech@gmail.com>
2017-03-19 14:28:51 +1100

    Added generate module

---
+++ logger/generate.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const pg = require('pg');
+const config = require('../config');
+
+module.exports = {
+  schema: (cb) => {
+    const ddl = fs.readFileSync('./ddl.sql', { encoding: 'utf8' });
+    const client = new pg.Client(config);
+    client.connect(err => {
+      if (err) { cb(err); }
+      client.query(ddl, (err, result) => {
+        if (err) { cb(err); }
+        if (result) {
+          client.end(err => {
+            if (err) throw err;
+            cb('ok');
+          });
+        }
+      });
+    });
+  }
+};
