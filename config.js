a33eee4a139044c1714ebbc8b69ae10990fe5564
Tim Douglas <neurotech@gmail.com>
2017-03-19 14:23:04 +1100

    Modularised config into module

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  host: process.env.MICROLOGGER_DB_HOST,
+  port: process.env.MICROLOGGER_DB_PORT,
+  user: process.env.MICROLOGGER_DB_USER,
+  password: process.env.MICROLOGGER_DB_PASSWORD,
+  database: process.env.MICROLOGGER_DB_NAME
+};
